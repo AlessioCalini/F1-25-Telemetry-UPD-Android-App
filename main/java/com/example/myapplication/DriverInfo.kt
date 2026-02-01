8f148dcec680b1c78b685b35ac8f279180a24ad6
AlessioCalini <81304505+AlessioCalini@users.noreply.github.com>
2026-02-02 00:11:52 +0100

    Delete main directory

--- main/java/com/example/myapplication/DriverInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication
-
-data class DriverInfo(
-    val name: String = "",
-    val delta: String = "",
-    val lastLapTime: String = "",     // ⭐ NUOVO: Last lap del pilota
-    val position: Int = 0
-)
